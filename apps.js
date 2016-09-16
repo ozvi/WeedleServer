71ec2b6e13d71ac2317173f80ca17c6888164f87
zvikachu <zviyemin@gmail.com>
2016-09-16 05:58:08 +0300

    f

--- apps.js
+++ apps.js
@@ -52,6 +52,7 @@ var upload = multer({ dest: '/tmp/'});
 
 // File input field name is simply 'file'
 app.post('/file_upload', upload.single('file'), function(req, res) {
+    console.log("on post image");
     var file = __dirname + '/' + req.file.filename;
     fs.rename(req.file.path, file, function(err) {
         if (err) {
