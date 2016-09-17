f31533ffa25a7cde5f05747f9ffa4390e32067cb
zvikachu <zviyemin@gmail.com>
2016-09-17 06:33:06 +0300

    added onNewGame()

--- apps.js
+++ apps.js
@@ -2,43 +2,22 @@
  * Created by zvi on 8/8/2016.
  */
 var express = require('express');
+
 var app = express();
 var requestIp = require('request-ip');
 var firebase = require('firebase');
-var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
 var fs = require('fs');
 const PORT = 9450;
-
-/*
-
 firebase.initializeApp({
-    serviceAccount: "./TEST3-c610d475e23d.json",
-    databaseURL: "https://test3-7d832.firebaseio.com"
+    serviceAccount: "./Weedle-69d94723eed7.json",
+    databaseURL: "https://weedle-27e37.firebaseio.com ",
     databaseAuthVariableOverride: {
         uid: "server_worker_1"
     }
 });
-
-/!**
- * This service worker should be the olny one getting access to database locations used by him
- * Example:
- * {
-  "rules": {
-    "serverControl": {
-      ".read": "auth.uid === 'server_worker_1'",
-      ".write": "auth.uid === 'server_worker_1'"
-    }
-  }
-}
- *!/
 var db = firebase.database();
-
-
-ITZIK!
-*/
-
-
 //start the node server
 app.set('port', process.env.PORT || PORT);
 
@@ -50,45 +29,71 @@ app.listen(app.get('port'),function(){
     console.log('server started on port itzik '+ app.get('port'));
 });
 
-// File input field name is simply 'file'
-
+// get winner social image file
 app.post('/file_upload', upload.single('png'), function (req, res, next) {
-        console.log('image file received!');
-})
+    console.log('image file received!');
+});
 
 
 
 
 
-/*
- var type = upload.single('recfile')
-app.post('/file_upload', type, function(req, res) {
-    console.log("on post image");
-    var targetPath = req.file.path;
 
-    var target_path = 'file_upload/' + req .file.originalname;
-    var src = fs.createReadStream(tmp_path);
 
-    var dest = fs.createWriteStream(target_path);
-    src.pipe(dest);
-    src.on('end', function() { res.render('complete'); });
-    src.on('error', function(err) { res.render('error'); });*/
 
-/*   var file = __dirname + '/' + req.file.filename;
-    fs.rename(req.file.path, file, function(err) {
-        if (err) {
-            console.log(err);
-            res.send(500);
-        } else {
-            res.json({
-                message: 'File uploaded successfully',
-                filename: req.file.filename
-            });
-            var stats =
-            console.log("file name: " + req.file.filename);
-        }
+
+var currentRunningGame = 0;
+pushNewGame(1);
+function pushNewGame(gameNum){
+
+    currentRunningGame++;
+    var gamesPresetsRef = db.ref("gamePresets/game"+currentRunningGame);
+    // Attach an asynchronous callback to read the data at our posts reference
+    console.log("game"+currentRunningGame);
+    gamesPresetsRef.once("value", function(snapshot) {
+        console.log(snapshot.val());
+        //set the current game to fb db
+        var gameObj = snapshot.val();
+        var gameRef = db.ref("gamePresets/game"+gameNum);
+        gameRef.update({
+            "backgroundUrl": gameObj.child("backgroundUrl").val(),
+            "gameSize": gameObj.child("gameSize").val(),
+            "prizeImageUrl": gameObj.child("prizeImageUrl").val(),
+            "prizeName": gameObj.child("prizeName").val(),
+            "startTimeMillis": calcNextGameStartTimeMillis(gameObj.child("minuteDelay").val()),
+            "medianBarPercent": 0
+        });
+        //start timer for game start
+        startGameTimer(gameObj.child("minuteDelay").val(),gameRef);
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
     });
-});*/
+
+};
+
+function startGameTimer (minutes, gameRef) {
+    console.log("timer start: " +minutes);
+    setTimeout(gameRef.update({
+        "gameRunning": true,
+        "winnerWon": false,
+        "winner": null,
+        "medianBarPercent": 0
+    }), minutes*60*1000);
+};
+
+function calcNextGameStartTimeMillis (minutes) {
+    var d = new Date();
+    var currentMillis = d.getTime();
+    var timeMillis = currentMillis+(minutes*60*1000);
+    return timeMillis;
+
+};
+
+
+
+
+
+
 
 
 
